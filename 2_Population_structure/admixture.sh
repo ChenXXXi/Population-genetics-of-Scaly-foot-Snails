f7fb5b7e55e9cbe2132b85dcd2627e9668d93db5
Xi Chen <87894889+ChenXXXi@users.noreply.github.com>
2025-06-15 05:33:10 +0800

    Create admixture.sh

---
+++ 2_Population_structure/admixture.sh
@@ -0,0 +1,4 @@
+nohup admixture --cv -j8 Csq.50kb.ld02.bed 2 > log2.out &
+nohup admixture --cv -j8 Csq.50kb.ld02.bed 3 > log3.out &
+nohup admixture --cv -j8 Csq.50kb.ld02.bed 4 > log4.out &
+nohup admixture --cv -j8 Csq.50kb.ld02.bed 5 > log5.out &
